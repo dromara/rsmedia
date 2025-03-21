4bb81f112fea1e29d2cf33b9ff41b231c8c516d3
admin <gyf@travelsky.com.cn>
2025-03-21 21:17:54 +0800

    build

---
+++ build.rs
@@ -0,0 +1,65 @@
+use std::env;
+
+fn main() {
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+
+    // common
+    println!("cargo:rustc-link-lib=dylib=c");
+
+    match target_os.as_str() {
+        "linux" => configure_linux(&target_arch),
+        "macos" | "darwin" => configure_macos(),
+        "windows" => configure_windows(),
+        _ => panic!("Unsupported operating system"),
+    }
+}
+
+fn configure_linux(target_arch: &str) {
+    println!("cargo:rustc-link-lib=dylib=dl");
+    println!("cargo:rustc-link-lib=dylib=pthread");
+
+    if target_arch == "x86_64" {
+        println!("cargo:rustc-link-search=native=/lib/x86_64-linux-gnu");
+        println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
+    } else if target_arch == "aarch64" {
+        println!("cargo:rustc-link-search=native=/lib/aarch64-linux-gnu");
+        println!("cargo:rustc-link-search=native=/usr/lib/aarch64-linux-gnu");
+    }
+
+    println!("cargo:rustc-link-search=native=/usr/local/lib");
+    println!("cargo:rustc-link-search=native=/usr/lib");
+    println!("cargo:rustc-link-search=native=/lib");
+}
+
+fn configure_macos() {
+    println!("cargo:rustc-link-lib=dylib=pthread");
+    println!("cargo:rustc-link-search=native=/usr/lib");
+    println!("cargo:rustc-link-search=native=/usr/local/lib");
+
+    // Support Homebrew
+    if std::path::Path::new("/opt/homebrew/lib").exists() {
+        // Apple Silicon (M1/M2)
+        println!("cargo:rustc-link-search=native=/opt/homebrew/lib");
+    }
+    if std::path::Path::new("/usr/local/opt").exists() {
+        // Intel
+        println!("cargo:rustc-link-search=native=/usr/local/opt");
+    }
+}
+
+fn configure_windows() {
+    if cfg!(target_env = "msvc") {
+        // MSVC
+        println!("cargo:rustc-link-search=native=C:\\Windows\\System32");
+        println!("cargo:rustc-link-lib=dylib=msvcrt");
+    } else {
+        // GNU (MinGW)
+        println!("cargo:rustc-link-lib=dylib=gcc_s");
+        println!("cargo:rustc-link-lib=dylib=pthread");
+        // MinGW lib path
+        if let Ok(gcc_dir) = env::var("MINGW_PREFIX") {
+            println!("cargo:rustc-link-search=native={}/lib", gcc_dir);
+        }
+    }
+}

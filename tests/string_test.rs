70ca2d6c10019852d835de43baf2d64e9f209a58
phial3 <feiyan.affection@gmail.com>
2025-04-04 18:48:39 +0800

    lints

--- tests/string_test.rs
+++ tests/string_test.rs
@@ -1,26 +1,32 @@
 #[test]
 fn main() {
     // 1. 普通字符串字面量 (String literals)
-    println!("1. &str: {:?}, {:?}, {:?}, {:?}", 'R', "foo", r"foo", r#"foo "bar" baz"#);
+    println!(
+        "1. &str: {:?}, {:?}, {:?}, {:?}",
+        'R', "foo", r"foo", r#"foo "bar" baz"#
+    );
     assert_eq!("foo", r"foo");
     assert_eq!("R", r"R");
     assert_eq!("\x52", "R");
     assert_eq!("foo \"bar\" baz", "foo \"bar\" baz");
     assert_eq!("foo \"bar\" baz", r#"foo "bar" baz"#);
-    assert_eq!("foo #\"# bar",  r##"foo #"# bar"##);
+    assert_eq!("foo #\"# bar", r##"foo #"# bar"##);
 
     // 2. 字节字符串字面量 (Byte string literals)
-    println!("2. Byte string &[u8]: {:?}, {:?}, {:?}, {:?}", b'R', b"foo", br"foo", br#"foo "bar" baz"#);
+    println!(
+        "2. Byte string &[u8]: {:?}, {:?}, {:?}, {:?}",
+        b'R', b"foo", br"foo", br#"foo "bar" baz"#
+    );
     assert_eq!(b'R', 82_u8);
     assert_eq!(br"foo", b"foo");
     assert_eq!(b"foo \"bar\" baz", br#"foo "bar" baz"#);
-    assert_eq!(b"foo #\"# bar",  br##"foo #"# bar"##);
+    assert_eq!(b"foo #\"# bar", br##"foo #"# bar"##);
     assert_eq!(b"R", br"R");
     assert_eq!(b"\x52", b"R");
     assert_eq!(b"\\x52", br"\x52");
 
     // 3. C字符串字面量 (C string literals)
-    println!("3. &CStr: {:?}, {:?}, {:?}",  c"foo", cr"foo", cr#""foo""#);
+    println!("3. &CStr: {:?}, {:?}, {:?}", c"foo", cr"foo", cr#""foo""#);
     assert_eq!(c"foo", cr"foo");
     assert_eq!(c"foo \"bar\" baz", cr#"foo "bar" baz"#);
     assert_eq!(c"\x52", c"R");
